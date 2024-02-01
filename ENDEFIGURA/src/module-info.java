4fdd4e7c084ec0868a86c5e6de874abd2b2cc7d7
orion <orion@DESKTOP-HUI8TTJ>
2024-02-01 19:42:16 +0100

    fgdfgfdg

--- ENDEFIGURA/src/module-info.java
+++ ENDEFIGURA/src/module-info.java
@@ -7,19 +7,16 @@
 module ENDEFIGURA {
 	//WEEE
 	//fkdhglksdflsdflsdflsdfj
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	// Branch Alejandro
 	//Branch 2
-=======
+
 	//sdfsdafsd
 	//rama aocheng
 	//dsfsdf
 	//xdfdgfdsfsd
 //sdfdsf
->>>>>>> refs/heads/rama_aocheng
-=======
-	
+
 	//dfsfsfsdfsdfsd
->>>>>>> refs/heads/Alejandro_2
+
 }
